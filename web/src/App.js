8e50990f9a44564bec885ec0204dd083dde77ed6
Eivind Melleby <emelleby@gmail.com>
2023-03-21 22:41:39 +0100

    Add tanstack queryClient

--- web/src/App.js
+++ web/src/App.js
@@ -1,3 +1,5 @@
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
@@ -7,12 +9,16 @@ import Routes from 'src/Routes'
 import './scaffold.css'
 import './index.css'
 
+const queryClient = new QueryClient()
+
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <RedwoodApolloProvider>
-        <Routes />
-      </RedwoodApolloProvider>
+      <QueryClientProvider client={queryClient}>
+        <RedwoodApolloProvider>
+          <Routes />
+        </RedwoodApolloProvider>
+      </QueryClientProvider>
     </RedwoodProvider>
   </FatalErrorBoundary>
 )
